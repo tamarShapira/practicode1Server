8bba078e0947c8f1db027edb77ab6d2d64bf4a8e
tamarShapira <atifal2022@gmail.com>
2023-11-05 23:37:26 +0200

    first commit in practicode1 server

---
+++ Program.cs
@@ -0,0 +1,71 @@
+using Microsoft.EntityFrameworkCore;
+using ToDoApi;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<ToDoDbContext>();
+builder.Services.AddCors();
+builder.Services.AddControllers();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+app.UseRouting(); 
+app.UseAuthorization(); 
+
+app.UseCors(builder => builder
+     .AllowAnyOrigin()
+     .AllowAnyMethod()
+     .AllowAnyHeader()); 
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseSwaggerUI(options =>
+{
+    options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+    options.RoutePrefix = string.Empty;
+});
+
+app.MapGet("/items", async (ToDoDbContext db) =>
+     await db.Items.ToListAsync());
+
+
+app.MapPost("/addTodo", async (Item todo, ToDoDbContext db) =>
+{
+    db.Items.Add(todo);
+    await db.SaveChangesAsync();
+
+    return Results.Created($"/todoitems/{todo.Id}", todo);
+});
+
+app.MapPut("/addTodo/{id}", async (int id, bool isComplete, ToDoDbContext db) =>
+{
+    var todo = await db.Items.FindAsync(id);
+
+    if (todo is null) return Results.NotFound();
+
+    todo.IsComplete = isComplete;
+
+    await db.SaveChangesAsync();
+
+    return Results.NoContent();
+});
+
+app.MapDelete("/deleteTodo/{id}", async (int id, ToDoDbContext db) =>
+{
+    if (await db.Items.FindAsync(id) is Item todo)
+    {
+        db.Items.Remove(todo);
+        await db.SaveChangesAsync();
+        return Results.NoContent();
+    }
+
+    return Results.NotFound();
+});
+
+app.Run();
